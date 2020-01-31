619d4c21ae41e2e7e2563a3cfedf83e31da58b5c
Gabriel Basilio Brito <gabrielbb0306@gmail.com>
2020-01-31 01:54:17 -0400

    Install "redhat.java" extension on downloaded vscode while doing integration tests

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -1,5 +1,6 @@
 import * as path from 'path';
-import { runTests } from 'vscode-test';
+import * as cp from 'child_process';
+import { runTests, downloadAndUnzipVSCode, resolveCliPathFromVSCodeExecutablePath } from 'vscode-test';
 
 (async () => {
     try {
@@ -11,8 +12,19 @@ import { runTests } from 'vscode-test';
         // Passed to --extensionTestsPath
         const extensionTestsPath = path.resolve(__dirname, './suite/index');
 
-        // Download VS Code, unzip it and run the integration test
-        await runTests({ extensionDevelopmentPath, extensionTestsPath });
+        // Download VS Code and unzip it
+        const vscodeExecutablePath = await downloadAndUnzipVSCode('1.41.1');
+
+        const cliPath = resolveCliPathFromVSCodeExecutablePath(vscodeExecutablePath);
+
+        // Install vscode-java extension to downloaded vscode
+        cp.spawnSync(cliPath, ['--install-extension', 'redhat.java'], {
+            encoding: 'utf-8',
+            stdio: 'inherit'
+        });
+
+        // run the integration test
+        await runTests({ vscodeExecutablePath, extensionDevelopmentPath, extensionTestsPath });
     } catch (err) {
         console.error('Failed to run tests');
         process.exit(1);
