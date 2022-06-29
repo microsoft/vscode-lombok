b4cf5dfc4d5d094b8b01a8c6f72d7e690f861366
Yan Zhang <yanzh@microsoft.com>
2022-06-29 16:03:01 +0800

    fix vulnerabilities
    Signed-off-by: Yan Zhang <yanzh@microsoft.com>

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -5,9 +5,11 @@ import * as glob from 'glob';
 export function run(): Promise<void> {
     // Create the mocha test
     const mocha = new Mocha({
-        ui: 'tdd'
+        ui: "tdd",
+        color: true,
+        timeout: 1 * 60 * 1000,
     });
-    mocha.useColors(true);
+
 
     const testsRoot = path.resolve(__dirname, '..');
 
